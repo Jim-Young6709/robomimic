25dcb752fe1e7bc98dbcf7a69ffb250f93c9efcd
Jiahui(Jim) Yang(Young) <jiahuiya@andrew.cmu.edu>
2025-04-08 17:55:31 -0400

    bugfix

--- robomimic/scripts/playback_dataset.py
+++ robomimic/scripts/playback_dataset.py
@@ -254,6 +254,7 @@ def playback_dataset(args):
         ObsUtils.initialize_obs_utils_with_obs_specs(obs_modality_specs=dummy_spec)
         env_meta = FileUtils.get_env_metadata_from_dataset(dataset_path=args.dataset)
         env_meta['env_kwargs']['cfg']['task']['setup_cameras'] = True if args.use_actions else False
+        env_meta['env_kwargs']['cfg']['task']['mp_kwargs']['camera_name'] = args.render_image_names[0]
         pcd_params = dict(num_robot_points=2048, num_obstacle_points=4096)
         env = EnvUtils.create_env_from_metadata(env_meta=env_meta, render=args.render, render_offscreen=write_video, pcd_params=pcd_params)
 
